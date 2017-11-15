37b7196a7470020d430e4b1c5722d8830e70876a
Ian Torres <itorres@umass.edu>
2017-11-15 13:02:39 -0500

    added: test_parse2-1.txt

--- parse_emily.py
+++ parse_emily.py
@@ -5,11 +5,38 @@ f = open("edCompleteFormatted.txt", 'r').read()
 
 tokens = nltk.word_tokenize(f)
 tagged_tokens = nltk.pos_tag(tokens)
+count_poem = 0
 
 freq_dict = dict()
+word_dict = dict()
 
 for token, pos in tagged_tokens:
     token_reg = token.lower().replace('-', '').replace('.', '')
+    if pos == 'CD':
+        count_poem += 1
+    else:
+        if re.compile(r'[a-z]+').match(token_reg) and not token_reg == 'is' and not pos == '``'\
+                and not token_reg == 'am' and not token_reg == 'are' and not token_reg == 'was'\
+                and not token_reg == 'were' and not token_reg == 'be' and not token_reg == 'being'\
+                and not token_reg == 'been' and not token_reg == 'have' and not token_reg == 'has'\
+                and not token_reg == 'had' and not token_reg == 'do' and not token_reg == 'does' \
+                and not token_reg == 'did' and not token_reg == 'shall' and not token_reg == 'will' \
+                and not token_reg == 'should' and not token_reg == 'would' and not token_reg == 'may' \
+                and not token_reg == 'might' and not token_reg == 'must' and not token_reg == 'can' \
+                and not token_reg == 'could' and not token_reg == "'":
+            if token_reg in word_dict:
+                if pos in word_dict[token_reg]['FREQ']:
+                    word_dict[token_reg]['FREQ'][pos] += 1
+                else:
+                    word_dict[token_reg]['POS'].append(pos)
+                    word_dict[token_reg]['FREQ'][pos] = 1
+            else:
+                word_dict[token_reg] = {'POS': [pos],
+                                        'FREQ': {
+                                            pos: 1
+                                         }
+                                        }
+
     if not pos == 'CD' and not pos == 'IN' and not pos == 'EX' \
             and not pos == 'LS' and not pos == "TO" and not pos == 'SYM' \
             and not pos == '.' and not pos == 'DT' and not pos == ','\
@@ -39,11 +66,24 @@ for token, pos in tagged_tokens:
                               }
 
 sorted_freq = sorted(freq_dict.items(), key=lambda e: sum(e[1]['FREQ'].values()), reverse=True)
+sorted_word = sorted(word_dict.items(), key=lambda e: sum(e[1]['FREQ'].values()), reverse=True)
+sorted_word = {word[0]: word[1] for word in sorted_word}
 
-print('\n\n')
+print('\n\n*************************Possible Seeds*************************\n\n')
 
 for freq in sorted_freq:
-    print(freq)
+    if len(freq[1]['POS']) >= 5:
+        print(freq)
+
+print('\n\n*************************Words*************************\n\n')
+
+for freq in sorted_word:
+    print(freq, ":", sorted_word[freq])
+
+print(count_poem)
+
+# for freq in sorted_freq:
+#     print(freq)
 
 # lines = []
 #
---
+++ parse_emily2.py
@@ -0,0 +1,89 @@
+import re
+import nltk
+
+f = open("edCompleteFormatted.txt", 'r').read()
+
+poem_dict = {i: poem.replace('\n', '').split(" ") for i, poem in enumerate(re.split(r'[0-9]+', f))}
+
+for poem in poem_dict:
+    for word in poem_dict[poem]:
+        if "." in word or "," in word or "?" in word or "!" in word or ";" in word:
+            correction = re.split(r'\.|,|\?|!', word)
+            if '' in correction:
+                if correction[0] == '':
+                    correction = [correction[1]]
+                else:
+                    correction = [correction[0]]
+            # print(correction)
+            poem_dict[poem].remove(word)
+            poem_dict[poem].extend(correction)
+        else:
+            if re.compile(r'[a-z]+[A-Z]+').match(word):
+                # print(word)
+                # print(re.search(r'[A-Z]', word))
+                indexL, _ = re.search(r'[A-Z]', word).span()
+                # print(index)
+                correction = [word[:indexL], word[indexL:]]
+                # print(correction)
+                poem_dict[poem].remove(word)
+                poem_dict[poem].extend(correction)
+            if re.compile(r'[A-Z][a-z]+[A-Z]').match(word):
+                # print(word)
+                # print(re.search(r'[a-z][A-Z]', word))
+                indexL, indexR = re.search(r'[a-z][A-Z]', word).span()
+                # print(index)
+                correction = [word[:indexR-1], word[indexL+1:]]
+                # print(correction)
+                poem_dict[poem].remove(word)
+                poem_dict[poem].extend(correction)
+
+proximity_dict = dict()
+
+for poem in poem_dict:
+    poem_words = poem_dict[poem]
+    for word in poem_words:
+        if not word == '':
+            if word not in proximity_dict:
+                proximity_dict[word] = dict()
+            for w in poem_words:
+                if not w == word:
+                    if w in proximity_dict[word]:
+                        if w in proximity_dict:
+                            if word in proximity_dict[w]:
+                                proximity_dict[w][word] += 1
+                            else:
+                                proximity_dict[w][word] = 1
+                        else:
+                            proximity_dict[w] = dict()
+                        proximity_dict[word][w] += 1
+                    else:
+                        if w in proximity_dict:
+                            if word in proximity_dict[w]:
+                                proximity_dict[w][word] += 1
+                            else:
+                                proximity_dict[w][word] = 1
+                        else:
+                            proximity_dict[w] = dict()
+                        proximity_dict[word][w] = 1
+
+
+# for line in f.split("\n"):
+#     words = line.split(" ")
+#     if re.compile(r'[a-zA-Z]+').match(line):
+#         print(line)
+#         for i, word in zip(range(len(words) - 1), words):
+#             if re.compile(r'[a-zA-Z]+').match(word):
+#                 if i == 0:
+#                     print(word, "_", words[i+1])
+#                 else:
+#                     print(words[i-1], "_", word, "_", words[i+1])
+
+sorted_word = sorted(proximity_dict.items(), key=lambda e: sum(e[1].values()), reverse=True)
+sorted_word = {word[0]: word[1] for word in sorted_word if not word[0] == ''}
+
+for word in sorted_word:
+    print(word, ":", len(sorted_word[word].keys()))
+    # print(word, ":\n", sorted_word[word].keys(), '\n')
+
+# for word in proximity_dict:
+#     print(word, ':', proximity_dict[word], '\n')
